8ab5534c3c15233cf9712682af19d544b0c36818
donaldgiang <donaldgiang26@gmail.com>
2026-05-19 16:57:24 -0700

    Import React in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect, useRef } from "react";
+import React from 'react'
+   import { useState, useEffect, useRef } from "react";
 
 /* ═══════════════════════════════════════════════════════════════════════════
    STYLES
